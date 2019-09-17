b628d0f7c1ee531f71f00b038f7d5d9625eee066
Marion Ivan Tan <marionltan@gmail.com>
2019-09-17 13:38:10 +0800

    added the scaffold for the buttons

--- src/example/src/main/java/com/aic/beacondetails/EmergencyButton.java
+++ src/example/src/main/java/com/aic/beacondetails/EmergencyButton.java
@@ -2,6 +2,7 @@ package com.aic.beacondetails;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmergencyButton extends AppCompatActivity {
 
@@ -9,6 +10,24 @@ public class EmergencyButton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_button);
+    }
+    public void btn1(View v){
+
+    }
+    public void btn2(View v){
+
+    }
+    public void btn3(View v){
+
+    }
+    public void btn4(View v){
+
+    }
+    public void btn5(View v){
+
+    }
+    public void btn6(View v){
+
     }
 
 }
