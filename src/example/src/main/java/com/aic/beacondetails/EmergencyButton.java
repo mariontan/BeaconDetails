c393531602f113fe3aa4b5a6f5efb3a375f46093
Marion Ivan Tan <marionltan@gmail.com>
2019-09-17 10:38:33 +0800

    added emergency button class

---
+++ src/example/src/main/java/com/aic/beacondetails/EmergencyButton.java
@@ -0,0 +1,14 @@
+package com.aic.beacondetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmergencyButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_button);
+    }
+
+}
