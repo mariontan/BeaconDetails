a8d7ef956c6fef0ff1430e90f6b8e9f5b76fed99
Marion Ivan Tan <marionltan@gmail.com>
2019-09-18 09:22:54 +0800

    moved GetSharedPref to BeaconState class

--- src/example/src/main/java/com/aic/beacondetails/EmergencyButton.java
+++ src/example/src/main/java/com/aic/beacondetails/EmergencyButton.java
@@ -3,31 +3,24 @@ package com.aic.beacondetails;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class EmergencyButton extends AppCompatActivity {
 
+    private BeaconState state = new BeaconState();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_button);
-    }
-    public void btn1(View v){
+        Button button = (Button) findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
-    public void btn2(View v){
 
-    }
-    public void btn3(View v){
 
-    }
-    public void btn4(View v){
-
-    }
-    public void btn5(View v){
-
-    }
-    public void btn6(View v){
-
-    }
 
 }
